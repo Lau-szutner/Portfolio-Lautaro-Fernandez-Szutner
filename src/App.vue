da38d12c73add62c0a652758a29e76eaa69ae39d
Lau-szutner <lautaroszutner@gmail.com>
2024-07-16 23:12:25 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from "./components/Navbar.vue";
+import Home from "./components/Home.vue";
+
+// Importar el CSS de forma estándar
+import "./assets/css/normalize.css";
+</script>
+
+<template>
+  <Navbar></Navbar>
+  <Home></Home>
+</template>
+
+<style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<script setup></script>
+
+<template>
+  <div id="contenedor" class="container-fluid bg-dark p-4">
+    <ul
+      class="list-unstyled d-flex gap-5 justify-content-center align-items-center m-0 text-white"
+    >
+      <li>Inicio</li>
+      <li>Quién soy</li>
+      <li>Proyectos</li>
+      <li>Habilidades</li>
+      <li>Contacto</li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+// .menu {
+//   color: white;
+//   list-style: none;
+//   display: flex;
+//   justify-content: flex-end; /* Alinea los elementos a la derecha */
+//   align-items: center; /* Centra verticalmente los elementos */
+//   height: 60px; /* Altura del menú */
+//   background-color: rgb(58, 17, 17);
+// }
+
+// .menu li {
+//   margin: 0 10px; /* Margen entre elementos del menú */
+//   cursor: pointer;
+// }
+
+// .menu li:hover {
+//   text-decoration: underline; /* Subraya el texto al pasar el mouse */
+// }
+
+// #contenedor {
+//   height: 1000px;
+//   background-color: aqua;
+// }
+</style>
