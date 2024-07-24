73888c8aeb4a3e6c804049ec07f0547602d91c87
Lau-szutner <lautaroszutner@gmail.com>
2024-07-23 21:50:52 -0300

    cambios menores

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import HomeInto from "./components/HomeIntro.vue";
-// import TrianglePattern from "./components/TrianglePattern.vue";
 
-// Importar el CSS de forma estándar
 import "./assets/css/normalize.css";
 </script>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,28 @@
 <script setup></script>
 
 <template>
-  <div id="contenedor" class="container-fluid p-4">
-    <ul
-      class="list-unstyled d-flex gap-5 justify-content-center align-items-center m-0"
-    >
-      <li>Inicio</li>
-      <li>Quién soy</li>
-      <li>Proyectos</li>
-      <li>Habilidades</li>
-      <li>Contacto</li>
-    </ul>
+  <div id="contenedor" class="container-fluid">
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills gap-5">
+        <li class="nav-item">
+          <a href="#" class="nav-link smooth-link" aria-current="page"
+            >Inicio</a
+          >
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link smooth-link">Quién soy</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link smooth-link">Proyectos</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link smooth-link">Habilidades</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link smooth-link">Contacto</a>
+        </li>
+      </ul>
+    </header>
   </div>
 </template>
 
@@ -19,12 +31,43 @@
   background-color: black;
 }
 
-#contenedor li {
+li {
   font-family: "Montserrat", sans-serif;
   font-optical-sizing: auto;
   font-style: normal;
   color: white;
   font-size: 2em;
   font-weight: 300;
+}
+
+.smooth-link {
+  position: relative;
+  color: white;
+  cursor: pointer; /* Opcional: para cambiar el cursor al estilo de un enlace */
+  transition: color 0.3s ease; /* Transición para el color del texto */
+}
+
+.smooth-link::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: -2px; /* Ajusta la posición vertical de la línea */
+  width: 100%;
+  height: 1px; /* Ajusta el grosor de la línea */
+  background: var(--header-color); /* Color de la línea */
+  transition: transform 0.3s ease; /* Transición para la línea */
+  transform: scaleX(0);
+  transform-origin: bottom right; /* Inicia la animación desde la derecha */
+}
+
+.smooth-link:hover::after {
+  transform: scaleX(1); /* Hace que la línea se expanda completamente */
+  transform-origin: bottom left; /* Anima hacia la izquierda */
+}
+
+.smooth-link:hover {
+  color: var(
+    --header-color
+  ); /* Opcional: Cambia el color del texto al pasar el cursor */
 }
 </style>
